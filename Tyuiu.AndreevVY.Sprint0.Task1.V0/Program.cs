e47e153247ec687addbc8d615c6bbe62d934d761
Владимир Андреев <djdfy1103@gmail.com>
2023-09-30 10:12:55 +0500

    Add project files.

---
+++ Tyuiu.AndreevVY.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AndreevVY.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Саша";
+            int age = 32;
+            Console.WriteLine("Привет, меня зовут " + name + " мне " + age + " года");
+            Console.ReadKey();
+        }
+    }
+}
