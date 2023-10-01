c88b3ff2b03855a7a6082de188f35e10df33287f
Владимир Андреев <djdfy1103@gmail.com>
2023-10-01 21:35:11 +0500

    Task5

---
+++ Tyuiu.AndreevVY.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AndreevVY.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.AndreevVY.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(2, 3));
+            Console.WriteLine("A - B = " + DataService.Subtraction(10, 3));
+            Console.WriteLine("A * b = " + DataService.Multiplicaton(12, 2));
+            Console.WriteLine("A / B = " + DataService.Division(10, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
